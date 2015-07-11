fe6c629c8cb19e05f210c96b2748cfb60ff0220e
Brandon Koepke <brandon.koepke@disney.com>
2015-07-10 17:17:08 -0700

    Changed max bandwidth to accomodate for 1080p.

--- src/com/cipherfish/xposed/org/linphone/LinphoneOverrides.java
+++ src/com/cipherfish/xposed/org/linphone/LinphoneOverrides.java
@@ -5,7 +5,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
-public class InCallActivity implements IXposedHookLoadPackage {
+public class LinphoneOverrides implements IXposedHookLoadPackage {
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 		if (! lpparam.packageName.equals("org.linphone"))
 			return;
@@ -13,6 +13,12 @@ public class InCallActivity implements IXposedHookLoadPackage {
 			@Override
 			protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 				param.setResult(false);
+			}
+		});
+		findAndHookMethod("org.linphone.LinphonePreferences", lpparam.classLoader, "setPreferredVideoSize", new XC_MethodHook() {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				param.args[0] = 2048;
 			}
 		});
 	}
